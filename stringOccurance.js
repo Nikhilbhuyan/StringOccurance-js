a9cc56dc3f13e4c80da86b81ad861e7558d32b8f
Nikhil Bhuyan <33249404+Nikhilbhuyan@users.noreply.github.com>
2018-06-16 17:37:48 +0530

    Add files via upload

---
+++ stringOccurance.js
@@ -0,0 +1,25 @@
+var run = function(){
+  var button = document.getElementById("btn");
+  var input = document.getElementById("input").value;
+  var div = document.createElement("div");
+  document.body.appendChild(div);
+
+  var occurance = 0;
+  input = input.toLowerCase();
+  var data = input.split(" ");
+  for( i = 0; i < data.length; i++){
+    for(j = 0; j < data.length; j++){
+      if(data[i] == data[j]){
+        occurance += 1;
+        if (occurance >= 2){
+          data.splice(j,1);
+        }
+      }
+    }
+    var p = document.createElement("p");
+    p.textContent = "The occurance of" +" " + data[i] +" "+ "is" +" "+ occurance;
+    div.appendChild(p);
+    occurance = 0;
+
+  }
+};
