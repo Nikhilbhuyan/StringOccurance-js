ec2de2e67237d9db2428cdbd133bfe7a60db61ef
Nikhil Bhuyan <33249404+Nikhilbhuyan@users.noreply.github.com>
2018-06-20 21:36:19 +0530

    added

---
+++ stringOccurance.js
@@ -0,0 +1,31 @@
+var name = prompt('what is your name');
+var head = document.createElement('h3');
+head.textContent = "Hii" + " " + name;
+document.body.appendChild(head);
+
+var run = function(){
+  var button = document.getElementById("btn");
+  var input = document.getElementById("input").value;
+  var div = document.createElement("div");
+  document.body.appendChild(div);
+
+  var occurance = 0;
+  input = input.toLowerCase();
+  var data = input.split(" ");
+  var len = data.length;
+  for( i = 0; i < len; i++){
+    for(j = 0; j < len; j++){
+      if(data[i] == data[j]){
+        occurance += 1;
+        if (occurance > 1){
+          data.splice(j,1);
+        }
+      }
+    }
+    var p = document.createElement("p");
+    p.textContent = "The occurance of" +" " + data[i] +" "+ "is" +" "+ occurance;
+    div.appendChild(p);
+    occurance = 0;
+
+  }
+};
